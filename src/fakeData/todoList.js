09e8cc67be4aec8b7449512d41f85088b4cde1fe
BaraKex <barzenkov.alexey@yandex.ru>
2018-09-05 11:56:11 +0300

    without vuex

---
+++ src/fakeData/todoList.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 0,
+        name: 'todo1',
+        comlated: true,
+    },
+    {
+        id: 1,
+        name: 'todo2',
+        comlated: false,
+    },
+    {
+        id: 2,
+        name: 'todo3',
+        comlated: false,
+    },
+];
